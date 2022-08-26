c8668ec84b5f7f5a89b5410c570bb97bf1533547
Srishty Bedi <70388131+srishty-07@users.noreply.github.com>
2022-08-26 15:02:28 +0530

    Update SectionTwo.jsx

--- src/TabComponents/SectionTwo.jsx
+++ src/TabComponents/SectionTwo.jsx
@@ -29,17 +29,18 @@ const SectionTwo = () => {
   }
   return (
     <div >
-      
-        <div style={{height:"100vh",marginTop:"120px"}}>
+      <div className="ghgheading" style={{ marginTop: "85px", marginLeft: "78px", textAlign: "center"}}>
+        <h1 style={{ fontSize: "60px", fontFamily: "Georgia, 'Times New Roman', Times, serif", color: "darkgreen" }}>CO2 Emission Trends <br></br>(कार्बन उत्सर्जन रुझान)</h1>
+      </div>
+        <div>
         <div className="container23" styles={{textAlign:"left"}}>
             <div className="c2">
-                <p className="para1" >The given dashboard will give you the values till 2025, to get the visualizations and predicted values for future year follow
-the following steps:
-                </p>
-                <p className='para1'>
+                <p className="para1" style={{textAlign:"center"}} >The given dashboard will give you the values till 2025, to get the visualizations and predicted values for future year follow the following instructions</p>
+                <h1 style={{marginLeft:"22px"}}>Instructions<br></br><span style={{fontSize:"20px",marginTop:"-20px"}}>(कैसे इस्तेमाल करे)</span></h1>
+                <ol>
                     <li>Enter the year you want the values to be predicted.</li>
                     <li>You will get the visualizations and the predicted values till that year.</li>
-                </p>
+                </ol>
             </div>
                
         </div>
@@ -51,7 +52,7 @@ the following steps:
                             </div>
                             <div className="col-75">
                                 <select id="country" name="country" onChange={handleCountryChange} value={selCountry!==""?selCountry:""}required>
-                                  <option value="" hidden>Country</option>
+                                  <option value="" hidden>Country (देश)</option>
                                   <option value="Canada">Canada</option>
                                   <option value="United States">United States</option>
                                   <option value="India">India</option>
@@ -67,7 +68,7 @@ the following steps:
                             </div>
                             <div className="col-75">
                                 <select id="reason" name="reason" onChange={handleCauseChange} value={selCause!==""?selCause:""} required>
-                                  <option value="" hidden>Cause</option>
+                                  <option value="" hidden>Cause (कार्बन उत्सर्जन का कारण)</option>
                                   {options}
                                 </select>
                                 <div className="invalid-feedback">
@@ -81,7 +82,7 @@ the following steps:
                             </div>
                             <div className="col-75">
                                 <select id="year" name="year" onChange={handleYearChange} value={selYear!==""?selYear:""} required>
-                                  <option value="" hidden>YYYY</option>
+                                  <option value="" hidden>YYYY (साल)</option>
                                   <option value="2019">2019</option>
                                   <option value="2020">2020</option>
                                   <option value="2021">2021</option>
@@ -97,7 +98,7 @@ the following steps:
                         </div>
                         
                             <div className="row" style={{marginTop:"12px"}}>
-                                <button className="btn btn-primary" type="submit" disabled={loading?true:false}>Predict</button>
+                                <button className="btn btn-primary" type="submit" disabled={loading?true:false}>Predict()</button>
                             </div>
                     </form>
                 </div>
